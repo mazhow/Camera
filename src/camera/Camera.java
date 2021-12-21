81cafdd1b14981b10045e05305de51aaf24119e0
mariehoward <marie.cockburn@gmail.com>
2021-12-21 12:20:21 +0000

    Further changes

--- src/camera/Camera.java
+++ src/camera/Camera.java
@@ -1,27 +1,43 @@
 package camera;
 
-public class Camera {
+public class Camera implements WriteListener{
 
     private Sensor sensor;
     private MemoryCard memoryCard;
     private byte[] data;
+    private boolean cameraOn;
+    private boolean isWriting;
 
-    public Camera(Sensor sensor, MemoryCard memoryCard) {
+    public Camera(Sensor sensor, MemoryCard memoryCard, byte[] data) {
         this.sensor = sensor;
         this.memoryCard = memoryCard;
+        this.data = data;
+        this.cameraOn = false;
     }
 
     public void pressShutter() {
-        //sensor.readData();
-        memoryCard.write(data);
+        if (cameraOn) {
+            isWriting = true;
+            memoryCard.write(data);
+        }
     }
 
     public void powerOn() {
+        cameraOn = true;
         sensor.powerUp();
     }
 
     public void powerOff() {
-       sensor.powerDown();
+        if (!isWriting) {
+            sensor.powerDown();
+        }
+        cameraOn = false;
+    }
+
+    @Override
+    public void writeComplete() {
+        isWriting = false;
+        sensor.powerDown();
     }
 }
 
--- test/camera/CameraTest.java
+++ test/camera/CameraTest.java
@@ -12,17 +12,17 @@ public class CameraTest {
 
     private final Sensor sensor = context.mock(Sensor.class);
     private final MemoryCard memoryCard = context.mock(MemoryCard.class);
-    Camera camera = new Camera(sensor, memoryCard);
+    private final WriteListener writeListener = context.mock(WriteListener.class);
+    private byte[] data = new byte[10];
+
+    Camera camera = new Camera(sensor, memoryCard, data);
 
     @Test
     public void switchingTheCameraOnPowersUpTheSensor() {
-
         context.checking(new Expectations() {{
             exactly(1).of(sensor).powerUp();
         }});
-
         camera.powerOn();
-
     }
 
     @Test
@@ -31,7 +31,6 @@ public class CameraTest {
         context.checking(new Expectations() {{
             exactly(1).of(sensor).powerDown();
         }});
-
         camera.powerOff();
     }
 
@@ -41,7 +40,6 @@ public class CameraTest {
         context.checking(new Expectations() {{
             ignoring(sensor).powerDown();
         }});
-
         camera.powerOff();
         camera.pressShutter();
     }
@@ -53,11 +51,39 @@ public class CameraTest {
 
         context.checking(new Expectations() {{
             ignoring(sensor).powerUp();
-            exactly(1).of(sensor).readData(); will(returnValue(data));
             exactly(1).of(memoryCard).write(data);
         }});
 
         camera.powerOn();
         camera.pressShutter();
     }
+
+    @Test
+    public void switchingOffCameraWhileDataBeingWrittenDoesNotPowerDownSensor() {
+
+        context.checking(new Expectations() {{
+            ignoring(sensor).powerUp();
+            ignoring(memoryCard);
+            never(sensor).powerDown();
+        }});
+
+        camera.powerOn();
+        camera.pressShutter();
+        camera.powerOff();
+    }
+
+    @Test
+    public void sensorPoweredDownOnceWritingIsComplete() {
+
+        context.checking(new Expectations() {{
+            ignoring(sensor).powerUp();
+            ignoring(writeListener);
+            exactly(1).of(sensor).powerDown();
+        }});
+
+        camera.powerOn();
+        camera.writeComplete();
+
+    }
+
 }
